d7e9ac74a994027a9f546ec84d8742c1913d3d6a
Sepehr <sepehrhadaeghnia@gmail.com>
2026-01-28 18:28:56 +0300

    chore: sync upcoming events for Jan 28

--- scripts/debug_char_codes.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const json = JSON.parse(fs.readFileSync(path.join(__dirname, 'houston_event.json'), 'utf8'));
-const timeStr = json.event_time;
-console.log('Time String:', timeStr);
-for (let i = 0; i < timeStr.length; i++) {
-    console.log(`Char ${i}: ${timeStr[i]} (${timeStr.charCodeAt(i)})`);
-}
